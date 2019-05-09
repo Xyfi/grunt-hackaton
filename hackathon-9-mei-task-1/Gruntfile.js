e892192d77104364dc56b8ffa28ab169c9e8c8b5
Alexander Botteram <a.botteram@gmail.com>
2019-05-09 11:03:48 +0200

    Finished task 1

--- hackathon-9-mei-task-1/Gruntfile.js
+++ hackathon-9-mei-task-1/Gruntfile.js
@@ -1,34 +1,61 @@
 /*global module:false*/
+const sass = require("node-sass");
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
+    assets: "./artifact/assets",
 
     // config
     copy: {
-      default: [ 'js', 'html' ],
+      default: [ "js", "html" ],
       js: {
         expand: true,
         src: "**/*.js",
-        dest: "./artifact/",
+        dest: "<%= assets %>",
         cwd: "source-files"
       },
       html: {
-        src: './index.html',
+        src: "./index.html",
         dest: "./artifact/",
       }
     },
     sass: {
-
+      options: {
+        implementation: sass,
+      },
+      default: {
+        files: {
+          "<%= assets %>/style.css": "./source-files/style.scss",
+        }
+      }
     },
+    compress: {
+      options: {
+        archive: "archive.zip",
+        mode: "zip",
+      },
+      default: {
+        files: [ {
+          expand: true,
+          cwd:  "./artifact/",
+          src: "**",
+          dest: "/",
+        } ],
+      }
+    },
+    clean: [ "artifact" ],
   });
 
   // Load tasks here.
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-compress");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-sass");
 
   // Define aliases here.
-  grunt.registerTask('default', ['copy']);
+  grunt.registerTask("default", ["copy", "sass", "compress", "clean"]);
 };
